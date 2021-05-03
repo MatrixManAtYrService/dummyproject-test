02d1e88e86fb68162535b369b3af5cddcf61fbae
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2021-05-03 15:36:51 -0600

    just formatting

--- fingerprints/fiftyrand.py
+++ fingerprints/fiftyrand.py
@@ -3,8 +3,10 @@ import subprocess
 import argparse
 from pathlib import Path
 
-parser = argparse.ArgumentParser(description="fingerprint with fifty pseudorandom inputs")
-parser.add_argument('projpath', help="a path to the repo under test")
+parser = argparse.ArgumentParser(
+    description="fingerprint with fifty pseudorandom inputs"
+)
+parser.add_argument("projpath", help="a path to the repo under test")
 args = parser.parse_args()
 
 project_path = Path(args.projpath)
@@ -14,7 +16,7 @@ random.seed(42)
 
 for _ in range(50):
     inval = str(random.randint(-100000, 100000))
-    script = str(project_path / 'addone.sh')
+    script = str(project_path / "addone.sh")
 
     # writes to stdout, which the TDID dag aggregates as a fingerprint
     process = subprocess.run([script, inval])
